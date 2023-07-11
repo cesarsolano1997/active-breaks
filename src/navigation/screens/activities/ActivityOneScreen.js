4526e34a42a8990fad3666bfd7a4148bcde76724
cesar.solano <cesar.solano@rpgo.pe>
2023-07-10 23:13:18 -0500

    GoBack

--- src/navigation/screens/activities/ActivityOneScreen.js
+++ src/navigation/screens/activities/ActivityOneScreen.js
@@ -14,6 +14,7 @@ import
 } from 'react-native'
 import React from 'react'
 import { windowWidth, windowHeight } from '../../../global/tools'
+import Stacks from '../../../global/stacks'
 
 const colors = {
   black: '#323F4E',
@@ -25,14 +26,14 @@ const timers = [...Array(13).keys()].map(i => (i === 0 ? 1 : i * 5))
 const ITEM_SIZE = windowWidth * 0.38
 const ITEM_SPACING = (windowWidth - ITEM_SIZE) / 2
 
-export default function ActivityOneScreen()
+export default function ActivityOneScreen({ navigation })
 {
   const scrollX = React.useRef(new Animated.Value(0)).current
   const [duration, setDuration] = React.useState(timers[0])
   const inputRef = React.useRef()
   const timerAnimation = React.useRef(new Animated.Value(windowHeight)).current
   const textInputAnimation = React.useRef(new Animated.Value(timers[0])).current
-  const buttonAnimation = React.useRef(new Animated.Value(0)).current  
+  const buttonAnimation = React.useRef(new Animated.Value(0)).current
 
   React.useEffect(() =>
   {
@@ -106,120 +107,128 @@ export default function ActivityOneScreen()
     outputRange: [0, 1]
   })
   return (
-    <View style={styles.container}>
-      <Animated.View
-        style={[
-          StyleSheet.absoluteFillObject, {
-            height: windowHeight,
-            width: windowWidth,
-            backgroundColor: 'red',
-            transform: [{
-              translateY: timerAnimation
-            }]
-          }
-        ]}
-      />
-      <Animated.View
-        style={[
-          StyleSheet.absoluteFillObject, {
-            justifyContent: 'flex-end',
-            alignItems: 'center',
-            paddingBottom: 100,
-            opacity,
-            transform: [{
-              translateY
-            }]
-          }
-        ]}
-      >
-        <TouchableOpacity
-          onPress={animation}
-        >
-          <View
-            style={styles.roundButton}
-          >
-          </View>
+    <>
+      <View style={{ backgroundColor: colors.black, 
+    paddingTop: StatusBar.currentHeight + 20}}>
+        <TouchableOpacity onPress={() => { navigation.navigate(Stacks.Main) }}>
+          <Text style={{ color: 'white', fontSize: 16 }}>{'<- Regresar al inicio'}</Text>
         </TouchableOpacity>
-      </Animated.View>
-      <View
-        style={{
-          position: 'absolute',
-          top: windowWidth / 3,
-          left: 0,
-          right: 0,
-          flex: 1
-        }}
-      >
+      </View>
+      <View style={styles.container}>
         <Animated.View
+          style={[
+            StyleSheet.absoluteFillObject, {
+              height: windowHeight,
+              width: windowWidth,
+              backgroundColor: 'red',
+              transform: [{
+                translateY: timerAnimation
+              }]
+            }
+          ]}
+        />
+        <Animated.View
+          style={[
+            StyleSheet.absoluteFillObject, {
+              justifyContent: 'flex-end',
+              alignItems: 'center',
+              paddingBottom: 100,
+              opacity,
+              transform: [{
+                translateY
+              }]
+            }
+          ]}
+        >
+          <TouchableOpacity
+            onPress={animation}
+          >
+            <View
+              style={styles.roundButton}
+            >
+            </View>
+          </TouchableOpacity>
+        </Animated.View>
+        <View
           style={{
             position: 'absolute',
-            justifyContent: 'center',
-            alignSelf: 'center',
-            alignItems: 'center',
-            opacity: textOpacity
+            top: windowWidth / 3,
+            left: 0,
+            right: 0,
+            flex: 1
           }}
         >
-          <TextInput
-            ref={inputRef}
-            style={styles.text}
-            defaultValue={duration.toString()}
-            editable={false}
+          <Animated.View
+            style={{
+              position: 'absolute',
+              justifyContent: 'center',
+              alignSelf: 'center',
+              alignItems: 'center',
+              opacity: textOpacity
+            }}
+          >
+            <TextInput
+              ref={inputRef}
+              style={styles.text}
+              defaultValue={duration.toString()}
+              editable={false}
+            />
+          </Animated.View>
+          <Animated.FlatList
+            data={timers}
+            keyExtractor={item => item.toString()}
+            horizontal
+            bounces={false}
+            onScroll={Animated.event(
+              [{
+                nativeEvent: {
+                  contentOffset: { x: scrollX }
+                }
+              }], { useNativeDriver: true }
+            )}
+            onMomentumScrollEnd={ev =>
+            {
+              const index = Math.floor(ev.nativeEvent.contentOffset.x / ITEM_SPACING)
+              setDuration(timers[index])
+            }}
+            showsHorizontalScrollIndicator={false}
+            snapToInterval={ITEM_SIZE}
+            contentContainerStyle={{
+              paddingHorizontal: ITEM_SPACING
+            }}
+            style={{ flexGrow: 0, opacity }}
+            renderItem={({ item, index }) =>
+            {
+              const inputRange = [
+                (index - 1) * ITEM_SIZE,
+                index * ITEM_SIZE,
+                (index + 1) * ITEM_SIZE
+              ]
+
+              const opacity = scrollX.interpolate({
+                inputRange,
+                outputRange: [.4, 1, .4]
+              })
+
+              const scale = scrollX.interpolate({
+                inputRange,
+                outputRange: [.7, 1, .7]
+              })
+              return <View style={{ width: ITEM_SIZE, justifyContent: 'center', alignItems: 'center' }}>
+                <Animated.Text style={[styles.text, {
+                  opacity,
+                  transform: [{
+                    scale
+                  }]
+                }]}>
+                  {item}
+                </Animated.Text>
+              </View>
+            }}
           />
-        </Animated.View>
-        <Animated.FlatList
-          data={timers}
-          keyExtractor={item => item.toString()}
-          horizontal
-          bounces={false}
-          onScroll={Animated.event(
-            [{
-              nativeEvent: {
-                contentOffset: { x: scrollX }
-              }
-            }], { useNativeDriver: true }
-          )}
-          onMomentumScrollEnd={ev =>
-          {
-            const index = Math.floor(ev.nativeEvent.contentOffset.x / ITEM_SPACING)
-            setDuration(timers[index])
-          }}
-          showsHorizontalScrollIndicator={false}
-          snapToInterval={ITEM_SIZE}
-          contentContainerStyle={{
-            paddingHorizontal: ITEM_SPACING
-          }}
-          style={{ flexGrow: 0, opacity }}
-          renderItem={({ item, index }) =>
-          {
-            const inputRange = [
-              (index - 1) * ITEM_SIZE,
-              index * ITEM_SIZE,
-              (index + 1) * ITEM_SIZE
-            ]
-
-            const opacity = scrollX.interpolate({
-              inputRange,
-              outputRange: [.4, 1, .4]
-            })
-
-            const scale = scrollX.interpolate({
-              inputRange,
-              outputRange: [.7, 1, .7]
-            })
-            return <View style={{ width: ITEM_SIZE, justifyContent: 'center', alignItems: 'center' }}>
-              <Animated.Text style={[styles.text, {
-                opacity,
-                transform: [{
-                  scale
-                }]
-              }]}>
-                {item}
-              </Animated.Text>
-            </View>
-          }}
-        />
+        </View>
       </View>
-    </View>
+    </>
   )
 }
 
