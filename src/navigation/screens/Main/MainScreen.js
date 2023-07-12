17959982a9aabed7ce11c083d00c7310846ad925
cesar.solano <cesar.solano@rpgo.pe>
2023-07-11 20:23:59 -0500

    Add new features

--- src/navigation/screens/Main/MainScreen.js
+++ src/navigation/screens/Main/MainScreen.js
@@ -1,56 +1,113 @@
-import { StyleSheet, Image, View, StatusBar, TouchableOpacity, Text } from 'react-native'
+import { StyleSheet, Image, View, StatusBar, TouchableOpacity, Text, Platform } from 'react-native'
 import React from 'react'
 import Stacks from '../../../global/stacks'
 import Modal from 'react-native-modal'
 import { FlatList } from 'react-native'
 import { windowWidth } from '../../../global/tools'
-
+import YoutubePlayer from 'react-native-youtube-iframe'
 const DATA = [{
     key: 1,
-    description: 'Levantate, estira los brazos hacia atrás y vuélvete a sentar(repetir)',
+    description: 'Estira frente al ordenador de 10 a 20 segundos 2 veces',
     image: require('../../../assets/step_1.png')
 }, {
     key: 2,
-    description: 'Estira el cuello hacia un lado y luego el otro(repetir)',
+    description: 'Estira la mano derecha y izquierda de 8 a 10 segundos',
     image: require('../../../assets/step_2.png')
 }, {
     key: 3,
-    description: 'Cruza un brazo por detrás de la cabeza y con el otro sujeta tu codo(repetir turnando ambos brazos)',
+    description: 'Mueve los hombros hacia arriba de 5 a 10 segundos 3 veces',
     image: require('../../../assets/step_3.png')
 }, {
     key: 4,
-    description: 'Une tus manos y estira tus brazos hacia arriba y bájalo(repetir)',
+    description: 'Pon la rodilla sobre la otra y mueve tu brazo hacia atrás de 8 a 10 segundos cada lado',
     image: require('../../../assets/step_4.png')
 }, {
     key: 5,
-    description: 'Une tus manos y estira tus brazos hacia un lado y luego hacia el otro lado(repetir)',
+    description: 'Estira la parte lumbar de tu cuerpo de 10 a 15 segundos 2 veces',
     image: require('../../../assets/step_5.png')
 }, {
     key: 6,
-    description: 'Ponte de pie, estira una pierna hacia atrás y regrésala al frente(repetir turnando ambas piernas)',
+    description: 'Levanta las manos de 10 a 15 segundos',
     image: require('../../../assets/step_6.png')
 }, {
     key: 7,
-    description: 'Apóyate en una silla e inclinate a 90 grados y levantate(repetir)',
+    description: 'Apóyate en una silla e inclínate a 90 grados y levántate(repetir)',
     image: require('../../../assets/step_7.png')
 }, {
     key: 8,
-    description: 'Sentado, sube y estira tus brazos al frente y bájalos(repetir)',
+    description: 'Estira los lados de los hombre durante 10 segundos cada lado ',
     image: require('../../../assets/step_8.png')
 }, {
     key: 9,
-    description: 'Sentado, sube una pierta y sujeta tu rodilla, bájala y cambia de pierna(repetir)',
+    description: 'Estira el cuello 10 a 20 segundos 2 veces',
     image: require('../../../assets/step_9.png')
+}]
+
+const DATA_2 = [{
+    key: 1,
+    description: 'Camina durante 1 minuto en tu oficina, pasillo o subiendo escaleras',
+    image: require('../../../assets/walk_1.png')
 }, {
-    key: 10,
-    description: 'Sentado, cruza una pierna y sujeta tu rodilla(repetir alternando ambos piernas)',
-    image: require('../../../assets/step_10.png')
+    key: 2,
+    description: 'Cuello: Inclinación de cuelo hacia los laterales y hacia delante de forma suave',
+    image: require('../../../assets/walk_2.png')
+}, {
+    key: 3,
+    description: 'Brazos y espalda: Puedes realizar varios de estos ejercicios, manteniendo la postura 10 segundos',
+    image: require('../../../assets/walk_3.png')
+}, {
+    key: 4,
+    description: 'Manos: Realiza estos dos ejercicios para elongar la musculatura de los flexores de los dedos',
+    image: require('../../../assets/walk_4.png')
+}, {
+    key: 5,
+    description: 'Durante 30 segundos realiza movimientos con los ojos para disminuir la fatiga visual: cierra los ojos varios segundos, mira a un lado y a otro, mira hacia el horizonte',
+    image: require('../../../assets/walk_5.png')
+}, {
+    key: 6,
+    description: 'Aprovecha que te has levantado para hidratarte',
+    image: require('../../../assets/walk_6.png')
+}]
+
+const DATA_3 = [{
+    key: 1,
+    description: 'Oscuridad total, tapa tus ojos',
+    image: require('../../../assets/visual_1.png')
+}, {
+    key: 2,
+    description: 'Derecha - izquierda - Izquierda - derecha',
+    image: require('../../../assets/visual_2.png')
+}, {
+    key: 3,
+    description: 'De esquina a esquina',
+    image: require('../../../assets/visual_3.png')
+}, {
+    key: 4,
+    description: 'Rotaciones',
+    image: require('../../../assets/visual_4.png')
+}, {
+    key: 5,
+    description: 'Arriba - abajo Abajo - arriba',
+    image: require('../../../assets/visual_5.png')
+}, {
+    key: 6,
+    description: 'Descanso',
+    image: require('../../../assets/visual_6.png')
 }]
 
 export default function MainScreen({ navigation })
 {
     const [visible, setVisible] = React.useState(false)
     const [step, setStep] = React.useState(1)
+
+    const [visible_2, setVisible_2] = React.useState(false)
+    const [step_2, setStep_2] = React.useState(1)
+
+    const [visible_3, setVisible_3] = React.useState(false)
+    const [step_3, setStep_3] = React.useState(1)
+
+
+    const [visible_4, setVisible_4] = React.useState(false)
 
     return (
         <View style={styles.container}>
@@ -80,8 +137,8 @@ export default function MainScreen({ navigation })
                     </TouchableOpacity>
                     <TouchableOpacity style={styles.activities} onPress={() =>
                     {
-                        setVisible(true)
-                            setStep(1)
+                        setVisible_2(true)
+                        setStep_2(1)
                     }}>
                         <Image
                             source={require('../../../assets/onboarding_2.jpg')}
@@ -94,11 +151,11 @@ export default function MainScreen({ navigation })
                     </TouchableOpacity>
                     <TouchableOpacity style={styles.activities} onPress={() =>
                     {
-                        setVisible(true)
-                        setStep(1)
+                        setVisible_3(true)
+                        setStep_3(1)
                     }}>
                         <Image
-                            source={require('../../../assets/onboarding_3.jpg')}
+                            source={require('../../../assets/card_3.jpg')}
                             style={{
                                 width: 150,
                                 height: 150,
@@ -111,8 +168,7 @@ export default function MainScreen({ navigation })
                 <View style={styles.right}>
                     <TouchableOpacity style={styles.activities} onPress={() =>
                     {
-                        setVisible(true)
-                        setStep(1)
+                        setVisible_4(true)
                     }}>
                         <Image
                             source={require('../../../assets/onboarding_4.jpg')}
@@ -170,14 +226,14 @@ export default function MainScreen({ navigation })
                                             alignItems: 'center'
                                         }}
                                     >
-
                                         <Image
                                             source={item.image}
                                             style={{
-                                                width: 250,
+                                                width: '100%',
                                                 height: 320,
                                                 marginHorizontal: 10
                                             }}
+                                            resizeMode='contain'
                                         />
 
                                         <Text style={{
@@ -205,6 +261,196 @@ export default function MainScreen({ navigation })
                             }}>
                             <Text style={{ color: 'white', fontFamily: 'Roboto', fontSize: 16 }}>Iniciar cronómetro</Text>
                         </TouchableOpacity>
+                    </View>
+                </View>
+            </Modal>
+
+            <Modal
+                isVisible={visible_2}
+                onBackdropPress={() => setVisible_2(false)}
+                animationIn={'slideInUp'}
+                animationOut={'slideOutDown'}
+                style={{ margin: 0, justifyContent: 'flex-end' }}
+                animationInTiming={800}
+                animationOutTiming={800}
+                onBackButtonPress={() => setVisible_2(false)}
+                onModalHide={() => setVisible_2(false)}
+                avoidKeyboard={true}
+            >
+                <View style={{ width: '100%', height: '75%', backgroundColor: 'white', borderTopLeftRadius: 10, borderTopRightRadius: 10 }}>
+                    <TouchableOpacity style={{ width: '100%', alignItems: 'flex-end', padding: 20, flexDirection: 'row' }}
+                        onPress={() => setVisible_2(false)}
+                    >
+                        <Text style={{ fontFamily: 'Roboto', fontSize: 16, fontWeight: '700', flex: 1 }}>Caminata en la oficina</Text>
+                        <Text style={{ fontWeight: 'bold', fontSize: 18 }}>X</Text>
+                    </TouchableOpacity>
+                    <View style={{ paddingHorizontal: 20 }}>
+                        <Text>Paso {step_2} de {DATA_2.length}</Text>
+                        <FlatList
+                            data={DATA_2}
+                            keyExtractor={item => item.key.toString()}
+                            horizontal
+                            showsHorizontalScrollIndicator={false}
+                            contentContainerStyle={{
+                                paddingHorizontal: 10
+                            }}
+                            onMomentumScrollEnd={ev =>
+                            {
+                                const index = Math.floor(ev.nativeEvent.contentOffset.x / windowWidth)
+                                setStep_2(index + 1)
+                            }}
+                            snapToInterval={windowWidth}
+                            renderItem={({ item, index }) =>
+                            {
+                                return (
+                                    <View
+                                        style={{
+                                            width: windowWidth,
+                                            justifyContent: 'center',
+                                            alignItems: 'center'
+                                        }}
+                                    >
+                                        <Image
+                                            source={item.image}
+                                            style={{
+                                                width: '100%',
+                                                height: 320,
+                                                marginHorizontal: 10
+                                            }}
+                                            resizeMode='contain'
+                                        />
+
+                                        <Text style={{
+                                            fontFamily: 'Roboto',
+                                            width: '90%',
+                                            marginTop: 10
+                                        }}>{item.description}</Text>
+                                    </View>
+                                )
+                            }}
+                        />
+                        <TouchableOpacity
+                            style={{
+                                borderRadius: 10,
+                                backgroundColor: '#1E232C',
+                                alignItems: 'center',
+                                height: 56,
+                                justifyContent: 'center',
+                                marginTop: 20
+                            }}
+                            onPress={() =>
+                            {
+                                setVisible(false)
+                                navigation.navigate(Stacks.ActivityOne)
+                            }}>
+                            <Text style={{ color: 'white', fontFamily: 'Roboto', fontSize: 16 }}>Iniciar cronómetro</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </Modal>
+
+            <Modal
+                isVisible={visible_3}
+                onBackdropPress={() => setVisible_3(false)}
+                animationIn={'slideInUp'}
+                animationOut={'slideOutDown'}
+                style={{ margin: 0, justifyContent: 'flex-end' }}
+                animationInTiming={800}
+                animationOutTiming={800}
+                onBackButtonPress={() => setVisible_3(false)}
+                onModalHide={() => setVisible_3(false)}
+                avoidKeyboard={true}
+            >
+                <View style={{ width: '100%', height: '60%', backgroundColor: 'white', borderTopLeftRadius: 10, borderTopRightRadius: 10 }}>
+                    <TouchableOpacity style={{ width: '100%', alignItems: 'flex-end', padding: 20, flexDirection: 'row' }}
+                        onPress={() => setVisible_3(false)}
+                    >
+                        <Text style={{ fontFamily: 'Roboto', fontSize: 16, fontWeight: '700', flex: 1 }}>Pausas visuales</Text>
+                        <Text style={{ fontWeight: 'bold', fontSize: 18 }}>X</Text>
+                    </TouchableOpacity>
+                    <View style={{ paddingHorizontal: 20 }}>
+                        <Text>Paso {step_3} de {DATA_3.length}</Text>
+                        <FlatList
+                            data={DATA_3}
+                            keyExtractor={item => item.key.toString()}
+                            horizontal
+                            showsHorizontalScrollIndicator={false}
+                            contentContainerStyle={{
+                                paddingHorizontal: 10
+                            }}
+                            onMomentumScrollEnd={ev =>
+                            {
+                                const index = Math.floor(ev.nativeEvent.contentOffset.x / windowWidth)
+                                setStep_2(index + 1)
+                            }}
+                            snapToInterval={windowWidth}
+                            renderItem={({ item, index }) =>
+                            {
+                                return (
+                                    <View
+                                        style={{
+                                            width: windowWidth,
+                                            justifyContent: 'center',
+                                            alignItems: 'center'
+                                        }}
+                                    >
+                                        <Image
+                                            source={item.image}
+                                            style={{
+                                                width: '100%',
+                                                height: 320,
+                                                marginHorizontal: 10
+                                            }}
+                                            resizeMode='contain'
+                                        />
+
+                                        <Text style={{
+                                            fontFamily: 'Roboto',
+                                            width: '90%',
+                                        }}>{item.description}</Text>
+                                    </View>
+                                )
+                            }}
+                        />
+                    </View>
+                </View>
+            </Modal>
+
+            <Modal
+                isVisible={visible_4}
+                onBackdropPress={() => setVisible_4(false)}
+                animationIn={'slideInUp'}
+                animationOut={'slideOutDown'}
+                style={{ margin: 0, justifyContent: 'flex-end' }}
+                animationInTiming={800}
+                animationOutTiming={800}
+                onBackButtonPress={() => setVisible_4(false)}
+                onModalHide={() => setVisible_4(false)}
+                avoidKeyboard={true}
+            >
+                <View style={{ width: '100%', height: '55%', backgroundColor: 'white', borderTopLeftRadius: 10, borderTopRightRadius: 10 }}>
+                    <TouchableOpacity style={{ width: '100%', alignItems: 'flex-end', padding: 20, flexDirection: 'row' }}
+                        onPress={() => setVisible_4(false)}
+                    >
+                        <Text style={{ fontFamily: 'Roboto', fontSize: 16, fontWeight: '700', flex: 1 }}>Pausas activas con baile</Text>
+                        <Text style={{ fontWeight: 'bold', fontSize: 18 }}>X</Text>
+                    </TouchableOpacity>
+                    <View style={{ paddingHorizontal: 20 }}>
+
+                        <YoutubePlayer
+                            height={200}
+                            play={true}
+                            videoId={'GmO1Uy4_pLM'}
+                            // onChangeState={onStateChange}
+                            // onReady={() => loadingRef?.current?.hide()}
+                            webViewStyle={{ opacity: 0.99 }}
+                            webViewProps={{
+                                renderToHardwareTextureAndroid: true,
+                                androidLayerType:
+                                    Platform.OS === 'android' && Platform.Version <= 22 ? 'hardware' : 'none'
+                            }}
+                        />
+                        <Text>En esta oportunidad se presenta a un experto en pausas activas con una linda dinámica de baile.</Text>
                     </View>
                 </View>
             </Modal>
